47e5d335356e7e80047e2f7f1718c50e9f436945
m.d.nicolae <n.mihaidavid@vidaxl.com>
2023-03-10 12:53:39 +0200

    Added network package

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "log"
+import (
+	"github.com/MihaiNicolae1/beginner-go-app/network"
+	"log"
+)
 
 func main() {
 	log.Println("Awesome CLI v0.01")
+	network.Ping("127.0.0.1")
 }
---
+++ network/ping.go
@@ -0,0 +1,8 @@
+package network
+
+import "log"
+
+// Ping - take an IP and ping an IP before returning response
+func Ping(ip string) {
+	log.Println(ip)
+}
